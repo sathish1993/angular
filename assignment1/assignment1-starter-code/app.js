b62e6f787ddc64c66b4101fb29eb5f57a99375db
sathish <sathish_kumar_93@yahoo.com>
2016-12-18 22:56:59 -0500

    first angular assignmnt

---
+++ assignment1/assignment1-starter-code/app.js
@@ -0,0 +1,38 @@
+(function () {
+  'use strict';
+
+  angular.module('LunchCheck',[])
+  .controller('LunchCheckController', LunchCheckController)
+
+  LunchCheckController.$injector = ['$scope'];
+  function LunchCheckController($scope) {
+    $scope.items = "";
+    $scope.itemCount = 0;
+    $scope.message = "";
+    $scope.color = "";
+    $scope.responseMessage = function () {
+      if($scope.items.length == 0){
+            $scope.message = "Please enter data first"
+            $scope.color = "red";
+      }else{
+            var count = 0;
+            var itemsSplit = $scope.items.split(",");
+            for(var i=0;i<itemsSplit.length;i++){
+              if(itemsSplit[i] !== ""){
+                count++;
+              }
+            }
+            $scope.itemCount = count;
+            console.log($scope.items);
+            console.log($scope.itemCount);
+            if($scope.itemCount <= 3 && $scope.itemCount >= 1){
+              $scope.message = "Enjoy!";
+              $scope.color = "green";
+            }else{
+              $scope.color = "green";
+              $scope.message = "Too much!";
+            }
+      }
+    }
+  }
+})();
