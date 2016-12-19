1a8b1bb996d48a7443ae660b462ca06af1649012
sathish <sathish_kumar_93@yahoo.com>
2016-12-19 03:18:06 -0500

    bulb

---
+++ bulbplay/app.js
@@ -0,0 +1,21 @@
+(function () {
+  'use strict'; //this checks if variable is initialsed or not, coz if used like x=1, wihtout var, then x takes global scope and causes problem later
+
+  angular.module('BulbApp',[])//module name and dependencies are the parameters
+  .controller('BulbController', BulbController)
+
+  BulbController.$injector = ['$scope'];
+  function BulbController($scope) {
+    $scope.currentState = 0;
+    $scope.state = "off";
+    $scope.switchBulb = function () {
+        if($scope.currentState === 0){
+          $scope.state = "off";
+          $scope.currentState = 1;
+        }else{
+          $scope.state = "on";
+          $scope.currentState = 0;
+        }
+      }
+    }
+})();
